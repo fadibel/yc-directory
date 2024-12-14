64b80fb51298072c87ebc487300b59db35d42cd1
text <fadibelkacemi05@gmail.com>
2024-12-14 13:18:37 +0100

    finish startup page

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,53 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   images: {
+//     dangerouslyAllowSVG: true,
+
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "*",
+//       },
+//     ],
+//   },
+
+//   // experimental: {
+//   //   ppr: "incremental",
+   
+//   // },
+//   devIndicators: {
+//     appIsrStatus: true,
+//     buildActivity: true,
+//     buildActivityPosition: "bottom-right",
+//   },
+// };
+
 const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     dangerouslyAllowSVG: true,
-    
     remotePatterns: [
       {
         protocol: "https",
         hostname: "*",
-        
       },
     ],
+  },
+  experimental: {
+    ppr: false,
+    after : true,
+    
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right",
   },
 };
 
